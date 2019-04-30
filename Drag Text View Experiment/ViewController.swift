a591098fd46f5cf854d81d0a874182de34b8b8b1
John Gallaugher <john.gallaugher@bc.edu>
2019-04-30 09:58:48 -0400

    Initial Commit

---
+++ Drag Text View Experiment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Drag Text View Experiment
+//
+//  Created by John Gallaugher on 4/30/19.
+//  Copyright © 2019 John Gallaugher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
